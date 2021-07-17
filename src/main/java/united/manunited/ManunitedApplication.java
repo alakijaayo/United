ccab3610973d222c02741c8fade41d89f6e6a24a
alakijaayo <alakijaayo@gmail.com>
2021-07-17 11:58:39 +0100

    first commit

---
+++ src/main/java/united/manunited/ManunitedApplication.java
@@ -0,0 +1,13 @@
+package united.manunited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManunitedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManunitedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/united/manunited/ManunitedApplicationTests.java
@@ -0,0 +1,13 @@
+package united.manunited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManunitedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
